31295967973f799c37a5abfe21b7f16c0df218de
Gary Baugh <baughg@tcd.ie>
2018-05-19 12:44:36 +0100

    added instruction set documentation

--- bench/mriscvcore.cpp
+++ bench/mriscvcore.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <cstdlib>
 #include <cstdio>
+#include <deque>
 
 Vmriscvcore *uut;                     // Instantiation of module
 vluint64_t main_time = 0;       // Current simulation time
@@ -48,6 +49,21 @@ int main(int argc, char** argv)
     uint32_t instr = 0;
     lui_instr lui_i;
     add_instr add_i;
+    std::deque<uint32_t> instructions;
+    lui_i.rd = 1;
+    lui_i.imm = 0x45;
+    instr = *((uint32_t*)&lui_i);
+    instructions.push_back(instr);
+    lui_i.rd = 2;
+    lui_i.imm = 0x67;
+    instr = *((uint32_t*)&lui_i);
+    instructions.push_back(instr);
+    add_i.rd = 3;
+    add_i.rs1 = 1;
+    add_i.rs2 = 2;
+    instr = *((uint32_t*)&add_i);
+    instructions.push_back(instr);
+
 
     while (!Verilated::gotFinish())
     {
@@ -61,32 +77,28 @@ int main(int argc, char** argv)
         {
             case 2:
             uut->rstn = 1;
+            uut->ARready = 0;
+            uut->Rvalid = 0;
             break;
 
-            case 4:
+            /*case 4:
             lui_i.rd = 1;
             lui_i.imm = 0x45;
             instr = *((uint32_t*)&lui_i);
             uut->Rdata = instr;
-            break;
-            case 5:
             uut->ARready = 1;
             uut->Rvalid = 1;
             break;
-            
-            case 10:
+                        
+            case 8:
             lui_i.rd = 2;
             lui_i.imm = 0x67;
             instr = *((uint32_t*)&lui_i);
             uut->Rdata = instr;
-            break;
-            case 11:
             uut->ARready = 1;
-            break;
-            case 12:
             uut->Rvalid = 1;
             break;
-
+            
             case 17:
             add_i.rd = 3;
             add_i.rs1 = 1;
@@ -105,9 +117,36 @@ int main(int argc, char** argv)
             default:
             uut->ARready = 0;
             uut->Rvalid = 0;
-            break;
+            break;*/
+        }
+        uut->eval();
+        
+          
+
+        if(cycle > 5 && pos_edge && instructions.size())
+        {
+            instr = instructions.front();
+            instructions.pop_front();
+
+            if(uut->ARvalid && uut->RReady)
+            {
+                uut->ARready = 1;
+                uut->Rvalid = 1;
+                uut->Rdata = instr;
+                printf("%05llu Read address: 0x%.8X\n",cycle,uut->ARdata);
+            }
+            else {
+                uut->ARready = uut->RReady;
+                uut->Rvalid = uut->RReady;
+                instructions.push_front(instr);
+            }
+        }
+        else if(pos_edge && !instructions.size()){
+            uut->ARready = 0;
+            uut->Rvalid = 0;
         }
 
+        
         uut->timestamp = cycle;
         uut->eval();            // Evaluate model
 
